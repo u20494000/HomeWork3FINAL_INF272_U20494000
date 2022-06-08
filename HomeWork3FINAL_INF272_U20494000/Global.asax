155393559fcf85e0667ef42b4519bb6ad8082b31
u20494000 <u20494000@tuks.co.za>
2022-06-08 20:03:41 +0200

    Add project files.

---
+++ HomeWork3FINAL_INF272_U20494000/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeWork3FINAL_INF272_U20494000.MvcApplication" Language="C#" %>
---
+++ HomeWork3FINAL_INF272_U20494000/Models/FileName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork3FINAL_INF272_U20494000.Models
+{
+    
+    public class FileModel
+    {
+        public string FileName { get; set; }
+        public string FileOption { get; set; }
+
+    }
+}
